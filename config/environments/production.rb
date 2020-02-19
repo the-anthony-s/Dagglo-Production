d0fdeb53d5eadaa7616397fa9b1fd1b1bdb3fc83
Anton S <antonsergeyev@me.com>
2020-02-19 04:54:47 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Compress JS using a preprocessor.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
