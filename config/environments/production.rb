5f5c34fae888bd73b5575e67fb131841cebda51e
Anton S <antonsergeyev@me.com>
2020-02-19 18:28:51 -0500

    uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,10 +39,11 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Compress JS using a preprocessor.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.compress = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
